20c6bddaddf91556a8765e57480a1592b8ff2705
Gustavo Beathyate <gustavo.bt@me.com>
2018-06-18 17:24:11 -0500

    Use config from config folder

--- lib/able_git_hooks/generators/install_generator.rb
+++ lib/able_git_hooks/generators/install_generator.rb
@@ -4,20 +4,23 @@ module AbleGitHooks
       desc "Install git hook files"
       source_root File.expand_path("../templates", __FILE__)
 
-      def copy_rubocop_files
-        copy_file "rubocop.yml",  ".rubocop.yml"
-        copy_file "rubocop_hook", "hooks/pre-commit/rubocop"
-        chmod     "0755",         "hooks/pre-commit/rubocop"
-      end
-
       def install_git_hooks
         copy_file "_do_hook", ".git/hooks/_do_hook"
-        chmod     "0755",     ".git/hooks/_do_hook"
+        chmod ".git/hooks/_do_hook", 0755
 
         AbleGitHooks::SCRIPTS.each do |hook|
           create_link ".git/hooks/#{hook}", "_do_hook"
           chmod       "0755",               ".git/hooks/#{hook}"
         end
+      end
+
+      def install_rubocop_hooks
+        copy_file "rubocop_hook", "hooks/pre-commit/rubocop"
+        chmod "hooks/pre-commit/rubocop", 0755
+      end
+
+      def copy_rubocop_defaults
+        template "rubocop.yml", "config/rubocop.yml"
       end
     end
   end
--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -13,15 +13,11 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   tests       AbleGitHooks::Generators::InstallGenerator
 
   def setup
-    remake_dirs ".git", "hooks"
+    remake_dirs ".git", "hooks", "config"
     run_generator
   end
 
-  def test_rubocop_files_were_copiead
-    assert_file ".rubocop.yml" do |f|
-      assert_match(/AllCops/, f)
-    end
-
+  def test_rubocop_hooks_were_copied
     assert_file "hooks/pre-commit/rubocop" do |f|
       assert_match(/bundle exec rubocop/, f)
     end
@@ -30,6 +26,12 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   def test_git_hook_was_copied
     assert_file ".git/hooks/_do_hook" do |f|
       assert_match(/for hook in/, f)
+    end
+  end
+
+  def test_rubocop_config_was_copied
+    assert_file "config/rubocop.yml" do |f|
+      assert_match(/AllCops/, f)
     end
   end
 
