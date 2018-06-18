d39c67ea4fba8ec4d28a6f914df5fae21bfb1437
Gustavo Beathyate <gustavo.bt@me.com>
2018-06-18 13:10:16 -0500

    Make scripts executable

--- lib/able_git_hooks/generators/install_generator.rb
+++ lib/able_git_hooks/generators/install_generator.rb
@@ -7,13 +7,16 @@ module AbleGitHooks
       def copy_rubocop_files
         copy_file "rubocop.yml",  ".rubocop.yml"
         copy_file "rubocop_hook", "hooks/pre-commit/rubocop"
+        chmod     0755,           "hooks/pre-commit/rubocop"
       end
 
       def install_git_hooks
         copy_file "_do_hook", ".git/hooks/_do_hook"
+        chmod     0755,       ".git/hooks/_do_hook"
 
         AbleGitHooks::SCRIPTS.each do |hook|
           create_link ".git/hooks/#{hook}", "_do_hook"
+          chmod       0755,                 ".git/hooks/#{hook}"
         end
       end
     end
